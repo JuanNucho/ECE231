d3a5e3608c820328edb6a84f28b9875789872737
Juan Muñoz <jmunoz81@unm.edu>
2024-09-17 23:34:05 -0600

    Removed mylib.c and mylib.h for Assignment 2

--- mylib.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYLIB_H   // Include guard to prevent multiple inclusion
-#define MYLIB_H
-
-int calculate_sum(int arr[], int size);
-float calculate_average(int arr[], int size);
-float calculate_std(int arr[], int size);
-
-#endif
